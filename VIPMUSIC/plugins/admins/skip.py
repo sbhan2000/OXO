cc1e849799cd26630c9ded9690a588494b4dc82e
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-12 00:04:17 +0300

    Update skip.py

--- VIPMUSIC/plugins/admins/skip.py
+++ VIPMUSIC/plugins/admins/skip.py
@@ -14,7 +14,7 @@ from config import BANNED_USERS
 
 
 @app.on_message(
-    filters.command(["skip", "تخطي", "next", "التالي"], prefixes=["/", "!", "%", ",",  ".", "@", "#"]) & ~BANNED_USERS
+    filters.command(["skip", "تخطي", "next", "التالي"], prefixes=["/", "!", "%", ",",  "", "@", "#"]) & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def skip(cli, message: Message, _, chat_id):
