e64b3e08a1c23c21b6d419ce4dafa2313e8b32d2
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-12 22:27:41 +0300

    Update reload.py

--- VIPMUSIC/plugins/tools/reload.py
+++ VIPMUSIC/plugins/tools/reload.py
@@ -30,7 +30,7 @@ rel = {}
 
 
 @app.on_message(
-    filters.command(["admincache", "reload", "refresh"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & filters.group & ~BANNED_USERS
+    filters.command(["admincache", "reload", "ريلود"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & filters.group & ~BANNED_USERS
 )
 @language
 async def reload_admin_cache(client, message: Message, _):
