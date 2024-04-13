3225e9c440ee49a0c8104a47b2c6f4a7847ae056
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-13 22:47:12 +0300

    Update مبرمج.py

--- VIPMUSIC/plugins/play/مبرمج.py
+++ VIPMUSIC/plugins/play/مبرمج.py
@@ -6,7 +6,6 @@ import aiohttp
 from pyrogram import filters
 from pyrogram import Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
-from strings.filters import command
 from VIPMUSIC import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 from VIPMUSIC import app
 from asyncio import gather
