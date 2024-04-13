d30de36a43f576946924155b362acbc0adb33840
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-13 22:05:23 +0300

    Create mute.py

---
+++ VIPMUSIC/plugins/admins/mute.py
@@ -0,0 +1,29 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from config import BANNED_USERS
+from strings import get_command
+from VIPMUSIC import app
+from VIPMUSIC.core.call import VIP
+from VIPMUSIC.utils.database import is_muted, mute_on
+from VIPMUSIC.utils.decorators import AdminRightsCheck
+
+# Commands
+MUTE_COMMAND = get_command("اسكت","/mute")
+
+
+@app.on_message(
+ filters.command(MUTE_COMMAND,"")
+    & ~BANNED_USERS)
+@AdminRightsCheck
+async def mute_admin(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1 or message.reply_to_message:
+        return await message.reply_text(_["general_2"])
+    if await is_muted(chat_id):
+        return await message.reply_text(_["admin_5"], disable_web_page_preview=True)
+    await mute_on(chat_id)
+    user_mention = message.from_user.mention if message.from_user else "المشـرف"
+    await VIP.mute_stream(chat_id)
+    await message.reply_text(
+        _["admin_6"].format(user_mention), disable_web_page_preview=True
+    )
