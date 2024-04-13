be6f752f59923856b4d4cd45fad5302a86adf87e
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-13 22:16:52 +0300

    Update unmute.py

--- VIPMUSIC/plugins/admins/unmute.py
+++ VIPMUSIC/plugins/admins/unmute.py
@@ -15,10 +15,10 @@ async def unmute_admin(Client, message: Message, _, chat_id):
     if not len(message.command) == 1 or message.reply_to_message:
         return await message.reply_text(_["general_2"])
     if not await is_muted(chat_id):
-        return await message.reply_text(_["admin_7"], disable_web_page_preview=True)
+        return await message.reply_text(_["admin_47"], disable_web_page_preview=True)
     await mute_off(chat_id)
     user_mention = message.from_user.mention if message.from_user else "المشـرف"
     await VIP.unmute_stream(chat_id)
     await message.reply_text(
-        _["admin_8"].format(user_mention), disable_web_page_preview=True
+        _["admin_48"].format(user_mention), disable_web_page_preview=True
     )
