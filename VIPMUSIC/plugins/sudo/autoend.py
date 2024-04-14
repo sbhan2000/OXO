4bbf4fb3d3fced41142ab04c7ba68c21cacb5c9a
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-14 13:25:02 +0300

    Update autoend.py

--- VIPMUSIC/plugins/sudo/autoend.py
+++ VIPMUSIC/plugins/sudo/autoend.py
@@ -12,12 +12,12 @@ async def auto_end_stream(_, message: Message):
     if len(message.command) != 2:
         return await message.reply_text(usage)
     state = message.text.split(None, 1)[1].strip().lower()
-    if state == "تفعيل":
+    if state == "enable":
         await autoend_on()
         await message.reply_text(
             "**🥤| تم تفعيل المغادرة التلقائية للمساعد .. بنجاح\n\nالحساب المساعد سوف يقوم بمغادرة الدردشة تلقائياً .. عند عدم وجود اشخاص في المكالمة**"
         )
-    elif state == "تعطيل":
+    elif state == "disable":
         await autoend_off()
         await message.reply_text("**🥤| تم تعطيل وضع المغادرة التلقائية للمساعد .. بنجاح.**")
     else:
