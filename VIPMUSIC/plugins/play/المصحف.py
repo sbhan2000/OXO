d4902477ff836f5591979c82c147cfc1d211c643
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-14 16:24:07 +0300

    Update المصحف.py

--- VIPMUSIC/plugins/play/المصحف.py
+++ VIPMUSIC/plugins/play/المصحف.py
@@ -107,7 +107,7 @@ async def show_quran(c,cq):
     per_name = quran[number]["sounds"][i]["name"]
     file = requests.get(quran[number]["sounds"][i]["url"]).content
     open(f"{name}.mp3","wb").write(file)
-    await app.send_audio(cq.message.chat.id,f"{name}.mp3",f"↯︙سورة {name} بصوت الشيخ {per_name} ",reply_markup = ikm([[ikb("‹ تشغيل ›", callback_data=f"done-{number}-{i}")]]))
+    await app.send_audio(cq.message.chat.id,f"{name}.mp3",f"**🥤| سورة {name} بصوت الشيخ {per_name}**",reply_markup = ikm([[ikb("‹ تشغيل ›", callback_data=f"done-{number}-{i}")]]))
 
 
 @app.on_callback_query(filters.regex("done") & ~BANNED_USERS)
