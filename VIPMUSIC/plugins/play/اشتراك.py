c26f4d63792a5716e0c4e47bc2bf0b9f8819ff14
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-15 23:33:08 +0300

    Update اشتراك.py

--- VIPMUSIC/plugins/play/اشتراك.py
+++ VIPMUSIC/plugins/play/اشتراك.py
@@ -1,13 +1,12 @@
-
-
-
+from pyrogram.enums import ChatType
 from config import MUST_JOIN
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+from VIPMUSIC import app
 
 
-@Client.on_message(filters.incoming & filters.private, group=-1)
+@app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
     if not MUST_JOIN:  # Not compulsory
         return
