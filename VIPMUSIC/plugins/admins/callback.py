db4bc528d427111983eeb18ed8ac82e66666383d
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-16 02:12:44 +0300

    Update callback.py

--- VIPMUSIC/plugins/admins/callback.py
+++ VIPMUSIC/plugins/admins/callback.py
@@ -412,7 +412,7 @@ async def del_back_playlist(client, CallbackQuery, _):
             except:
                 return await CallbackQuery.message.reply_text(_["call_6"])
             button = stream_markup(_, videoid, chat_id)
-            img = await get_thumb(videoid)
+            img = await get_thumb(videoid, photo)
             run = await CallbackQuery.message.reply_photo(
                 photo=img,
                 caption=_["stream_1"].format(
@@ -448,7 +448,7 @@ async def del_back_playlist(client, CallbackQuery, _):
             except:
                 return await mystic.edit_text(_["call_6"])
             button = stream_markup(_, videoid, chat_id)
-            img = await get_thumb(videoid)
+            img = await get_thumb(videoid, photo)
             run = await CallbackQuery.message.reply_photo(
                 photo=img,
                 caption=_["stream_1"].format(
@@ -519,7 +519,7 @@ async def del_back_playlist(client, CallbackQuery, _):
                 db[chat_id][0]["markup"] = "tg"
             else:
                 button = stream_markup(_, videoid, chat_id)
-                img = await get_thumb(videoid)
+                img = await get_thumb(videoid, photo)
                 run = await CallbackQuery.message.reply_photo(
                     photo=img,
                     caption=_["stream_1"].format(
