aa8c2e22e777e110d8939d5fe5736609b156ebb7
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-15 13:35:14 +0300

    Update منوع.py

--- VIPMUSIC/plugins/play/منوع.py
+++ VIPMUSIC/plugins/play/منوع.py
@@ -401,3 +401,25 @@ async def game_7(client, message):
 
 
 
+@app.on_message(filters.command(["حديث","احاديث"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]))
+async def game_7(client, message):
+   f = "TheIslamicProphet"
+   t = message.chat.id
+   d = randint(2,190)
+   await app.copy_message(
+      t,
+      f,
+      d,
+      reply_to_message_id=message.id,
+      reply_markup=InlineKeyboardMarkup(
+      [
+      [
+      InlineKeyboardButton(message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+      ]
+      ]
+      )
+   )
+
+
+
+
