c0e695c37c0d77b5a4833cf98c5506284dd30acd
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-15 20:59:33 +0300

    Create السورس.py

---
+++ VIPMUSIC/plugins/play/السورس.py
@@ -0,0 +1,41 @@
+import asyncio
+from pyrogram import Client, filters
+from strings import get_string
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, ReplyKeyboardMarkup
+from VIPMUSIC import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from random import choice
+from pyrogram import filters
+from config import BANNED_USERS
+from config import OWNER_ID
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import (InlineKeyboardButton,CallbackQuery,
+                            InlineKeyboardMarkup, Message)
+from typing import Union
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove, Message, ChatJoinRequest
+
+
+REPLY_MESSAGE = "**🥤| اهلا بك عزيزي العضو في قسم السورس**"
+REPLY_MESSAGE_BUTTONS = [
+    [
+             ("المبرمج")
+          ],
+          [
+             ("السورس"),
+              ("المطور")
+          ],
+          [
+             ("رجوع")
+    ]
+]
+
+@app.on_message(filters.regex("^قسم السورس"))
+async def cpanel(_, message: Message):             
+        text = REPLY_MESSAGE
+        reply_markup = ReplyKeyboardMarkup(REPLY_MESSAGE_BUTTONS, resize_keyboard=True, selective=True)
+        await message.reply(
+              text=text,
+              reply_markup=reply_markup
+        )
+
+
+
