92ae6b1eeebc0104166b5167db7255cd23d28085
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-15 15:19:26 +0300

    Update منوع.py

--- VIPMUSIC/plugins/play/منوع.py
+++ VIPMUSIC/plugins/play/منوع.py
@@ -487,7 +487,7 @@ async def game_7(client, message):
 async def game_7(client, message):
    f = "tatqem"
    t = message.chat.id
-   d = randint(2,190)
+   d = randint(2,70)
    await app.copy_message(
       t,
       f,
@@ -508,7 +508,7 @@ async def game_7(client, message):
 async def game_7(client, message):
    f = "ramad_any"
    t = message.chat.id
-   d = randint(2,190)
+   d = randint(2,100)
    await app.copy_message(
       t,
       f,
