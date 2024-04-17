34b5b6593e387316eb7a9a2afca7870eba75d83a
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-17 20:07:12 +0300

    Create مصحف.py

---
+++ VIPMUSIC/plugins/play/مصحف.py
@@ -0,0 +1,62 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup as ikm, InlineKeyboardButton as ikb
+from VIPMUSIC import app 
+import json 
+from config import BANNED_USERS
+import requests 
+from VIPMUSIC.core.call import VIP
+import asyncio
+import os
+import time
+import requests
+from config import START_IMG_URL
+import random
+from VIPMUSIC import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from VIPMUSIC import app
+from random import  choice, randint
+import asyncio, redis, requests, datetime, random
+
+
+
+
+@app.on_message(filters.regex("^ختمة"))
+        voic = ["مرحبا بك في قسم المصحف الرجاء ارسال رقم الصفحة لتصفح صفحات القرآن الكريم للرجوع ارسل /start",]
+        bot.send_message(call.message.chat.id,voic)
+@bot.message_handler(func=lambda message: True)
+def all(message):
+    try:
+            num = int(message.text)
+            url = "https://quran.ksu.edu.sa/png_big/" + str(num) + ".png"
+
+            keyboard = types.InlineKeyboardMarkup()
+            cou = types.InlineKeyboardButton(text=f"• {num} •", callback_data="couu")
+            previous = types.InlineKeyboardButton(text="صفحة السابقة", callback_data=str(num - 1))
+            next = types.InlineKeyboardButton(text="صفحة التالية", callback_data=str(num + 1))
+
+            keyboard.row(cou)
+            keyboard.row(previous,next)
+
+            bot.send_photo(message.chat.id,url, reply_markup=keyboard)
+    except:
+            pass
+            bot.reply_to(message,'error')
+
+@bot.callback_query_handler(func=lambda call: True)
+def alll(call):
+    if call.data == 'couu':
+     bot.answer_callback_query(call.id, text='هذا زر يعرض فيه العدد فقط')
+     exit()
+    num = int(call.data)
+    url = "https://quran.ksu.edu.sa/png_big/" + str(num) + ".png"
+
+    keyboard = types.InlineKeyboardMarkup()
+
+    cou = types.InlineKeyboardButton(text=f"• {num} •", callback_data="couu")
+    previous = types.InlineKeyboardButton(text="صفحة السابقة", callback_data=str(num - 1))
+    next = types.InlineKeyboardButton(text="صفحة التالية", callback_data=str(num + 1))
+
+
+    keyboard.row(cou)
+    keyboard.row(previous,next)
+
+    bot.edit_message_media(types.InputMediaPhoto(url), call.message.chat.id, call.message.message_id,reply_markup=keyboard)           
