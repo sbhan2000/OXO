01c5db9da1d6308b9e78b0ada580cf67abc91209
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-17 03:00:23 +0300

    Update stop.py

--- VIPMUSIC/plugins/admins/stop.py
+++ VIPMUSIC/plugins/admins/stop.py
@@ -20,5 +20,5 @@ async def stop_music(cli, message: Message, _, chat_id):
     await set_loop(chat_id, 0)
     user_mention = message.from_user.mention if message.from_user else "المشـرف"
     await message.reply_text(
-        _["admin_5"].format(user.mention), reply_markup=close_markup(_)
+        _["admin_5"].format(user_mention), reply_markup=close_markup(_)
     )
