339b7c154ea779809b1a41cf5ed85d6d06ee1a0d
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-18 15:49:56 +0300

    Update مشرف.py

--- VIPMUSIC/plugins/play/مشرف.py
+++ VIPMUSIC/plugins/play/مشرف.py
@@ -88,7 +88,7 @@ def promote_g_admin(client, message):
     for tom in tooom:
     	if tom.user.id == tom_id and (tom.status == enums.ChatMemberStatus.OWNER or tom.status == enums.ChatMemberStatus.ADMINISTRATOR):
     		client.promote_chat_member(chat_id, user_id, ToM)
-    		message.reply(f"**🥤| تم رفع {user_id} مشرف بنجاح.**")
+    		message.reply(f"**🥤| تم رفع العضو مشرف بنجاح.**")
     		
     		
     		
