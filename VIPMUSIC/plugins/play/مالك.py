82a391742b0d80acda6f701e4529ab603f693124
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-19 16:26:53 +0300

    Create مالك.py

---
+++ VIPMUSIC/plugins/play/مالك.py
@@ -0,0 +1,42 @@
+import asyncio
+import os
+import time
+import requests
+import aiohttp
+from pyrogram import filters
+from pyrogram import Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from VIPMUSIC import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from VIPMUSIC import app
+from telegraph import upload_file
+from asyncio import gather
+from pyrogram.errors import FloodWait
+
+#       #             #  #####  #####      ####
+#        #           #  #         #            #     #
+#          #        #  #####  #            #####    
+#           #    #    #          #     ##   #     #
+#              #      #####   ######   #     #
+
+
+
+
+@app.on_message(filters.command(["المالك", "المنشئ", "المنشي"]) & filters.group)
+async def gak_owne(client: Client, message: Message):
+      if len(message.command) >= 2:
+         return 
+      else:
+            chat_id = message.chat.id
+            f = "administrators"
+            async for member in client.iter_chat_members(chat_id, filter=f):
+               if member.status == "creator":
+                 id = member.user.id
+                 key = InlineKeyboardMarkup([[InlineKeyboardButton(member.user.first_name, user_id=id)]])
+                 m = await client.get_chat(id)
+                 if m.photo:
+                       photo = await app.download_media(m.photo.big_file_id)
+                       return await message.reply_photo(photo, caption=f"🧞‍♂️ ¦𝙺𝙸𝙽𝙶 :{m.first_name}\n🎯 ¦𝚄𝚂𝙴𝚁 :@{m.username}\n🎃 ¦𝙸𝙳 :`{m.id}`\n💌 ¦𝙱𝙸𝙾 :{m.bio}\n✨ ¦𝙲𝙷𝙰𝚃: {message.chat.title}\n♻️ ¦𝙸𝙳.𝙲𝙷𝙰𝚃 :`{message.chat.id}`",reply_markup=key)
+                 else:
+                    return await message.reply("• " + member.user.mention)
+                    
+                   
