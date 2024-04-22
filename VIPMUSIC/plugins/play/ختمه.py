87cc54758c19bbf8b699bea41d38736d45ffeca7
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-22 18:27:31 +0300

    Update ختمه.py

--- VIPMUSIC/plugins/play/ختمه.py
+++ VIPMUSIC/plugins/play/ختمه.py
@@ -27,6 +27,9 @@ async def handle_message(client, message):
 
 @app.on_message(filters.regex("^الختمه"))
 async def handle_message(client, message):
+    text = message.text
+    chat_id = message.chat.id
+    msg_id = message.message_id
         keyboard = InlineKeyboardMarkup([
             [InlineKeyboardButton("البدء من جديد", callback_data="restart")],
             [InlineKeyboardButton("استئناف الختمه", callback_data="resume")]
