00eb4a3c0e8ef5bf88428491cf34be3afcb4448b
sbhan2000 <166266081+sbhan2000@users.noreply.github.com>
2024-04-22 20:25:45 +0300

    Update ختمه.py

--- VIPMUSIC/plugins/play/ختمه.py
+++ VIPMUSIC/plugins/play/ختمه.py
@@ -25,53 +25,91 @@ bot = Client(
 
 @app.on_message(filters.regex("^الختمه"))
 async def handle(client, message):
-        keyboard = InlineKeyboardMarkup([
-            [InlineKeyboardButton("البدء من جديد", callback_data="restart")],
-            [InlineKeyboardButton("استئناف الختمه", callback_data="resume")]
-        ])
-        await message.reply_text("اختر احدى الازرار", reply_markup=keyboard)
-    
-@app.on_callback_query()
-async def handle_callback(client, callback_query):
-    data = callback_query.data
-    chat_id = callback_query.message.chat.id
+        reply_markup = {
+        "type": "inline",
+        "data": [
+            [
+                {"text": "البدء من جديد", "data": f"{msg.sender_id.user_id}/restas"},
+                {"text": "استئناف الختمه", "data": f"{msg.sender_id.user_id}/estisaf"}
+            ]
+        ]
+    }
+    return send("اختر احدى الازرار", reply_markup)
 
-    if data == "restart":
-        readable = 0
-        photo = "https://quran.ksu.edu.sa/png_big/1.png"
-        caption = "الصفحه 1"
-        keyboard = InlineKeyboardMarkup([
-            [InlineKeyboardButton(" التالي ", callback_data="next")],
-            [InlineKeyboardButton(" السابق ", callback_data="prev")]
-        ])
-        await bot.send_photo(chat_id, photo, caption=caption, reply_markup=keyboard)
+# callback
 
-    elif data == "next":
-        readable += 1
-        photo = f"https://quran.ksu.edu.sa/png_big/{readable}.png"
-        caption = f'الصفحه {readable}'
-        keyboard = InlineKeyboardMarkup([
-            [InlineKeyboardButton(" التالي ", callback_data="next")],
-            [InlineKeyboardButton(" السابق ", callback_data="prev")]
-        ])
-        await bot.send_photo(chat_id, photo, caption=caption, reply_markup=keyboard)
+if Text and Text.startswith(r'(\d+)/restas'):  # البدء من جديد / أول صفحه
+    Redis.delete(f"{TheMero}:readables:{IdUser}")
+    photo = "https://quran.ksu.edu.sa/png_big/1.png"
+    caption = "الصفحه 1"
+    readable = 0
+    ratpep = readable + 1
+    Redis.set(f"{TheMero}:readables:{IdUser}", int(ratpep))
+    keyboard = {
+        "inline_keyboard": [
+            [{"text": " التالي ", "callback_data": "/nextts"}],
+            [{"text": " السابق ", "callback_data": "/priors"}]
+        ]
+    }
+    msg_rep = msg.id / 2097152 / 0.5
+    urllib.request.urlopen(f"https://api.telegram.org/bot{Token}/sendphoto?chat_id={msg.chat_id}&reply_to_message_id={msg_rep}&photo={photo}&caption={urllib.parse.quote(caption)}&parse_mode=markdown&disable_web_page_preview=true&reply_markup={json.dumps(keyboard)}")
 
-    elif data == "prev":
-        readable -= 1
-        photo = f"https://quran.ksu.edu.sa/png_big/{readable}.png"
-        caption = f'الصفحه {readable}'
-        keyboard = InlineKeyboardMarkup([
-            [InlineKeyboardButton(" التالي ", callback_data="next")],
-            [InlineKeyboardButton(" السابق ", callback_data="prev")]
-        ])
-        await bot.send_photo(chat_id, photo, caption=caption, reply_markup=keyboard)
+if Text == "/nextts":  # التالي
+    readablet = int(Redis.get(f"{TheMero}:readables:{IdUser}") or 0)
+    if readablet == 604:  # اخر صفحه
+        return False
+    ratpep = readablet + 1
+    Redis.set(f"{TheMero}:readables:{IdUser}", int(ratpep))
+    media = {
+        "type": "photo",
+        "media": f"https://quran.ksu.edu.sa/png_big/{int(ratpep)}.png",
+        "caption": f'الصفحه {int(ratpep)} ',
+        "parse_mode": "Markdown"
+    }
+    keyboard = {
+        "inline_keyboard": [
+            [{"text": " التالي ", "callback_data": "/nextts"}],
+            [{"text": " السابق ", "callback_data": "/priors"}]
+        ]
+    }
+    msg_rep = Msg_id / 2097152 / 0.5
+    urllib.request.urlopen(f"http://api.telegram.org/bot{Token}/editmessagemedia?chat_id={ChatId}&message_id={msg_rep}&media={json.dumps(media)}&reply_markup={json.dumps(keyboard)}")
 
-    elif data == "resume":
-        readable = 0
-        photo = f"https://quran.ksu.edu.sa/png_big/{readable}.png"
-        caption = f'الصفحه {readable}'
-        keyboard = InlineKeyboardMarkup([
-            [InlineKeyboardButton(" التالي ", callback_data="next")],
-            [InlineKeyboardButton(" السابق ", callback_data="prev")]
-        ])
-        await bot.send_photo(chat_id, photo, caption=caption, reply_markup=keyboard)
+if Text == "/priors":  # السابق
+    readablet = int(Redis.get(f"{TheMero}:readables:{IdUser}") or 2)
+    if readablet == 1:
+        return False
+    ratpep = readablet - 1
+    Redis.set(f"{TheMero}:readables:{IdUser}", int(ratpep))
+    media = {
+        "type": "photo",
+        "media": f"https://quran.ksu.edu.sa/png_big/{int(ratpep)}.png",
+        "caption": f'الصفحه {int(ratpep)} ',
+        "parse_mode": "Markdown"
+    }
+    keyboard = {
+        "inline_keyboard": [
+            [{"text": " التالي ", "callback_data": "/nextts"}],
+            [{"text": " السابق ", "callback_data": "/priors"}]
+        ]
+    }
+    msg_rep = Msg_id / 2097152 / 0.5
+    urllib.request.urlopen(f"http://api.telegram.org/bot{Token}/editmessagemedia?chat_id={ChatId}&message_id={msg_rep}&media={json.dumps(media)}&reply_markup={json.dumps(keyboard)}")
+
+if Text and Text.startswith(r'(\d+)/estisaf'):  # استئناف
+    readablet = int(Redis.get(f"{TheMero}:readables:{IdUser}") or 1)
+    photo = f"https://quran.ksu.edu.sa/png_big/{readablet}.png"
+    caption = f"الصفحه {readablet}"
+    readable = 1
+    ratpep = readable + 1
+    Redis.set(f"{TheMero}:readables:{IdUser}", int(ratpep))
+    keyboard = {
+        "inline_keyboard": [
+            [{"text": " التالي ", "callback_data": "/nextts"}],
+            [{"text": " السابق ", "callback_data": "/priors"}]
+        ]
+    }
+    msg_rep = msg.id / 2097152 / 0.5
+    urllib.request.urlopen(f"https://api.telegram.org/bot{Token}/sendphoto?chat_id={msg.chat_id}&reply_to_message_id={msg_rep}&photo={photo}&caption={urllib.parse.quote(caption)}&parse_mode=markdown&disable_web_page_preview=true&reply_markup={json.dumps(keyboard)}")
+
+        
